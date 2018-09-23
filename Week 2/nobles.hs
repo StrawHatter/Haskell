659390abc56d03a98a03e3d2cf9731b16bbf16c8
Paul Dreczkowski <StrawHatter@users.noreply.github.com>
2018-09-24 00:36:35 +0100

    Update nobles.hs

--- Week 2/nobles.hs
+++ Week 2/nobles.hs
@@ -2,3 +2,9 @@
 
 mknoble :: Bool -> String -> String
 mknoble female name = (if female then "Dame " else "Sir ") ++ name
+
+-- Ashley is cute
+-- Speller is wack
+
+speller :: [[Char]] -> [Char]
+speller = (intercalate ", ") . map (\w@(x:_) -> (x : " is for ") ++ w)
